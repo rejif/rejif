33f955b99af33ea0a93907800cb82d7c4bdd78a7
onoie <onoie3@gmail.com>
2018-06-12 18:50:57 +0900

    rm comment

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,88 +1,25 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
 
 #include <QtWidgets>
 
 #include "mainwindow.h"
 #include "scribblearea.h"
 
-//! [0]
-MainWindow::MainWindow()
-{
+MainWindow::MainWindow(){
     scribbleArea = new ScribbleArea;
     setCentralWidget(scribbleArea);
-
     createActions();
     createMenus();
-
     setWindowTitle(tr("Scribble"));
     resize(500, 500);
 }
-//! [0]
-
-//! [1]
-void MainWindow::closeEvent(QCloseEvent *event)
-//! [1] //! [2]
-{
+void MainWindow::closeEvent(QCloseEvent *event){
     if (maybeSave()) {
         event->accept();
     } else {
         event->ignore();
     }
 }
-//! [2]
-
-//! [3]
-void MainWindow::open()
-//! [3] //! [4]
-{
+void MainWindow::open(){
     if (maybeSave()) {
         QString fileName = QFileDialog::getOpenFileName(this,
                                    tr("Open File"), QDir::currentPath());
@@ -90,46 +27,24 @@ void MainWindow::open()
             scribbleArea->openImage(fileName);
     }
 }
-//! [4]
-
-//! [5]
-void MainWindow::save()
-//! [5] //! [6]
-{
+void MainWindow::save(){
     QAction *action = qobject_cast<QAction *>(sender());
     QByteArray fileFormat = action->data().toByteArray();
     saveFile(fileFormat);
 }
-//! [6]
-
-//! [7]
-void MainWindow::penColor()
-//! [7] //! [8]
-{
+void MainWindow::penColor(){
     QColor newColor = QColorDialog::getColor(scribbleArea->penColor());
     if (newColor.isValid())
         scribbleArea->setPenColor(newColor);
 }
-//! [8]
-
-//! [9]
-void MainWindow::penWidth()
-//! [9] //! [10]
-{
+void MainWindow::penWidth(){
     bool ok;
-    int newWidth = QInputDialog::getInt(this, tr("Scribble"),
-                                        tr("Select pen width:"),
-                                        scribbleArea->penWidth(),
-                                        1, 50, 1, &ok);
-    if (ok)
+    int newWidth = QInputDialog::getInt(this, tr("Scribble"),tr("Select pen width:"),scribbleArea->penWidth(),1, 50, 1, &ok);
+    if (ok){
         scribbleArea->setPenWidth(newWidth);
+    }
 }
-//! [10]
-
-//! [11]
-void MainWindow::about()
-//! [11] //! [12]
-{
+void MainWindow::about(){
     QMessageBox::about(this, tr("About Scribble"),
             tr("<p>The <b>Scribble</b> example shows how to use QMainWindow as the "
                "base widget for an application, and how to reimplement some of "
@@ -143,86 +58,58 @@ void MainWindow::about()
                "how to use QPainter to draw an image in real time, as well as "
                "to repaint widgets.</p>"));
 }
-//! [12]
-
-//! [13]
-void MainWindow::createActions()
-//! [13] //! [14]
-{
+void MainWindow::createActions(){
     openAct = new QAction(tr("&Open..."), this);
     openAct->setShortcuts(QKeySequence::Open);
     connect(openAct, SIGNAL(triggered()), this, SLOT(open()));
-
     foreach (QByteArray format, QImageWriter::supportedImageFormats()) {
         QString text = tr("%1...").arg(QString(format).toUpper());
-
         QAction *action = new QAction(text, this);
         action->setData(format);
         connect(action, SIGNAL(triggered()), this, SLOT(save()));
         saveAsActs.append(action);
     }
-
     printAct = new QAction(tr("&Print..."), this);
     connect(printAct, SIGNAL(triggered()), scribbleArea, SLOT(print()));
-
     exitAct = new QAction(tr("E&xit"), this);
     exitAct->setShortcuts(QKeySequence::Quit);
     connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
-
     penColorAct = new QAction(tr("&Pen Color..."), this);
     connect(penColorAct, SIGNAL(triggered()), this, SLOT(penColor()));
-
     penWidthAct = new QAction(tr("Pen &Width..."), this);
     connect(penWidthAct, SIGNAL(triggered()), this, SLOT(penWidth()));
-
     clearScreenAct = new QAction(tr("&Clear Screen"), this);
     clearScreenAct->setShortcut(tr("Ctrl+L"));
-    connect(clearScreenAct, SIGNAL(triggered()),
-            scribbleArea, SLOT(clearImage()));
-
+    connect(clearScreenAct, SIGNAL(triggered()),scribbleArea, SLOT(clearImage()));
     aboutAct = new QAction(tr("&About"), this);
     connect(aboutAct, SIGNAL(triggered()), this, SLOT(about()));
-
     aboutQtAct = new QAction(tr("About &Qt"), this);
     connect(aboutQtAct, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
 }
-//! [14]
-
-//! [15]
-void MainWindow::createMenus()
-//! [15] //! [16]
-{
+void MainWindow::createMenus(){
     saveAsMenu = new QMenu(tr("&Save As"), this);
-    foreach (QAction *action, saveAsActs)
+    foreach (QAction *action, saveAsActs){
         saveAsMenu->addAction(action);
-
+    }
     fileMenu = new QMenu(tr("&File"), this);
     fileMenu->addAction(openAct);
     fileMenu->addMenu(saveAsMenu);
     fileMenu->addAction(printAct);
     fileMenu->addSeparator();
     fileMenu->addAction(exitAct);
-
     optionMenu = new QMenu(tr("&Options"), this);
     optionMenu->addAction(penColorAct);
     optionMenu->addAction(penWidthAct);
     optionMenu->addSeparator();
     optionMenu->addAction(clearScreenAct);
-
     helpMenu = new QMenu(tr("&Help"), this);
     helpMenu->addAction(aboutAct);
     helpMenu->addAction(aboutQtAct);
-
     menuBar()->addMenu(fileMenu);
     menuBar()->addMenu(optionMenu);
     menuBar()->addMenu(helpMenu);
 }
-//! [16]
-
-//! [17]
-bool MainWindow::maybeSave()
-//! [17] //! [18]
-{
+bool MainWindow::maybeSave(){
     if (scribbleArea->isModified()) {
        QMessageBox::StandardButton ret;
        ret = QMessageBox::warning(this, tr("Scribble"),
@@ -238,14 +125,8 @@ bool MainWindow::maybeSave()
     }
     return true;
 }
-//! [18]
-
-//! [19]
-bool MainWindow::saveFile(const QByteArray &fileFormat)
-//! [19] //! [20]
-{
+bool MainWindow::saveFile(const QByteArray &fileFormat){
     QString initialPath = QDir::currentPath() + "/untitled." + fileFormat;
-
     QString fileName = QFileDialog::getSaveFileName(this, tr("Save As"),
                                initialPath,
                                tr("%1 Files (*.%2);;All Files (*)")
@@ -257,4 +138,3 @@ bool MainWindow::saveFile(const QByteArray &fileFormat)
         return scribbleArea->saveImage(fileName, fileFormat.constData());
     }
 }
-//! [20]
--- scribblearea.cpp
+++ scribblearea.cpp
@@ -1,52 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
 
 #include <QtWidgets>
 #if defined(QT_PRINTSUPPORT_LIB)
@@ -59,26 +10,18 @@
 
 #include "scribblearea.h"
 
-//! [0]
-ScribbleArea::ScribbleArea(QWidget *parent)
-    : QWidget(parent)
-{
+ScribbleArea::ScribbleArea(QWidget *parent):QWidget(parent){
     setAttribute(Qt::WA_StaticContents);
     modified = false;
     scribbling = false;
     myPenWidth = 1;
     myPenColor = Qt::blue;
 }
-//! [0]
-
-//! [1]
-bool ScribbleArea::openImage(const QString &fileName)
-//! [1] //! [2]
-{
+bool ScribbleArea::openImage(const QString &fileName){
     QImage loadedImage;
-    if (!loadedImage.load(fileName))
+    if (!loadedImage.load(fileName)){
         return false;
-
+    }
     QSize newSize = loadedImage.size().expandedTo(size());
     resizeImage(&loadedImage, newSize);
     image = loadedImage;
@@ -86,15 +29,9 @@ bool ScribbleArea::openImage(const QString &fileName)
     update();
     return true;
 }
-//! [2]
-
-//! [3]
-bool ScribbleArea::saveImage(const QString &fileName, const char *fileFormat)
-//! [3] //! [4]
-{
+bool ScribbleArea::saveImage(const QString &fileName, const char *fileFormat){
     QImage visibleImage = image;
     resizeImage(&visibleImage, size());
-
     if (visibleImage.save(fileName, fileFormat)) {
         modified = false;
         return true;
@@ -102,72 +39,39 @@ bool ScribbleArea::saveImage(const QString &fileName, const char *fileFormat)
         return false;
     }
 }
-//! [4]
-
-//! [5]
-void ScribbleArea::setPenColor(const QColor &newColor)
-//! [5] //! [6]
-{
+void ScribbleArea::setPenColor(const QColor &newColor){
     myPenColor = newColor;
 }
-//! [6]
-
-//! [7]
-void ScribbleArea::setPenWidth(int newWidth)
-//! [7] //! [8]
-{
+void ScribbleArea::setPenWidth(int newWidth){
     myPenWidth = newWidth;
 }
-//! [8]
-
-//! [9]
-void ScribbleArea::clearImage()
-//! [9] //! [10]
-{
+void ScribbleArea::clearImage(){
     image.fill(qRgb(255, 255, 255));
     modified = true;
     update();
 }
-//! [10]
-
-//! [11]
-void ScribbleArea::mousePressEvent(QMouseEvent *event)
-//! [11] //! [12]
-{
+void ScribbleArea::mousePressEvent(QMouseEvent *event){
     if (event->button() == Qt::LeftButton) {
         lastPoint = event->pos();
         scribbling = true;
     }
 }
-
-void ScribbleArea::mouseMoveEvent(QMouseEvent *event)
-{
+void ScribbleArea::mouseMoveEvent(QMouseEvent *event){
     if ((event->buttons() & Qt::LeftButton) && scribbling)
         drawLineTo(event->pos());
 }
-
-void ScribbleArea::mouseReleaseEvent(QMouseEvent *event)
-{
+void ScribbleArea::mouseReleaseEvent(QMouseEvent *event){
     if (event->button() == Qt::LeftButton && scribbling) {
         drawLineTo(event->pos());
         scribbling = false;
     }
 }
-
-//! [12] //! [13]
-void ScribbleArea::paintEvent(QPaintEvent *event)
-//! [13] //! [14]
-{
+void ScribbleArea::paintEvent(QPaintEvent *event){
     QPainter painter(this);
     QRect dirtyRect = event->rect();
     painter.drawImage(dirtyRect, image, dirtyRect);
 }
-//! [14]
-
-//! [15]
-void ScribbleArea::resizeEvent(QResizeEvent *event)
-//! [15] //! [16]
-{
+void ScribbleArea::resizeEvent(QResizeEvent *event){
     if (width() > image.width() || height() > image.height()) {
         int newWidth = qMax(width() + 128, image.width());
         int newHeight = qMax(height() + 128, image.height());
@@ -176,48 +80,29 @@ void ScribbleArea::resizeEvent(QResizeEvent *event)
     }
     QWidget::resizeEvent(event);
 }
-//! [16]
-
-//! [17]
-void ScribbleArea::drawLineTo(const QPoint &endPoint)
-//! [17] //! [18]
-{
+void ScribbleArea::drawLineTo(const QPoint &endPoint){
     QPainter painter(&image);
-    painter.setPen(QPen(myPenColor, myPenWidth, Qt::SolidLine, Qt::RoundCap,
-                        Qt::RoundJoin));
+    painter.setPen(QPen(myPenColor, myPenWidth, Qt::SolidLine, Qt::RoundCap,Qt::RoundJoin));
     painter.drawLine(lastPoint, endPoint);
     modified = true;
-
     int rad = (myPenWidth / 2) + 2;
-    update(QRect(lastPoint, endPoint).normalized()
-                                     .adjusted(-rad, -rad, +rad, +rad));
+    update(QRect(lastPoint, endPoint).normalized().adjusted(-rad, -rad, +rad, +rad));
     lastPoint = endPoint;
 }
-//! [18]
-
-//! [19]
-void ScribbleArea::resizeImage(QImage *image, const QSize &newSize)
-//! [19] //! [20]
-{
-    if (image->size() == newSize)
+void ScribbleArea::resizeImage(QImage *image, const QSize &newSize){
+    if (image->size() == newSize){
         return;
-
+    }
     QImage newImage(newSize, QImage::Format_RGB32);
     newImage.fill(qRgb(255, 255, 255));
     QPainter painter(&newImage);
     painter.drawImage(QPoint(0, 0), *image);
     *image = newImage;
 }
-//! [20]
-
-//! [21]
-void ScribbleArea::print()
-{
+void ScribbleArea::print(){
 #if QT_CONFIG(printdialog)
     QPrinter printer(QPrinter::HighResolution);
-
     QPrintDialog printDialog(&printer, this);
-//! [21] //! [22]
     if (printDialog.exec() == QDialog::Accepted) {
         QPainter painter(&printer);
         QRect rect = painter.viewport();
@@ -229,4 +114,3 @@ void ScribbleArea::print()
     }
 #endif // QT_CONFIG(printdialog)
 }
-//! [22]
